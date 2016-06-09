fca052ea0edfb2cfe3a2919563548960691ad4bd
Fizal Sarif <fizal.sarif@gmail.com>
2016-06-09 17:35:22 -0400

    fixed screen

--- js/app.js
+++ js/app.js
@@ -2,9 +2,25 @@ $(function() {
   console.log('loaded');
 
   // various variables
-
+  var game = true;
 
   // function to spawn the aliens
+  function alienSpawn() {
+    var img = $('<img>');
+    img.attr('class', 'alien');
+    img.attr('src', "http://rs345.pbsrc.com/albums/p386/WackoWeasel/invader1.gif~c100");
+
+    var posX = Math.floor(Math.random() * ($('.gameScreen').width() - 32));
+    var posY = Math.floor(Math.random() * ($('.gameScreen').height() - 32));
+    img.css('left', posX + 'px');
+    img.css('top', posY + 'px');
+
+    $(img).appendTo('.gameScreen').fadeIn(500).delay(2500).fadeOut(500, function() {
+      $(this).remove();
+    });
+  }
+
+
 
 
   // their keypress function
@@ -22,6 +38,9 @@ $(function() {
   // function to update current color
 
 
+
+  // testing grounds
+  setInterval(alienSpawn, 1000);
 
 
 });
