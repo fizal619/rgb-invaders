34158f57f3406efa94b8b14f8da962203da1aaff
Fizal Sarif <fizal.sarif@gmail.com>
2016-06-09 15:05:10 -0400

    planned out what needs to be done from a 30,000ft view

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-$(function(){
+$(function() {
   console.log('loaded');
+
+  // various variables
+
+
+  // function to spawn the aliens
+
+
+  // their keypress function
+
+
+  // function to generate the random color
+
+
+  // function to update score or end game if 0
+
+
+  // function to fill bars
+
+
+  // function to update current color
+
+
+
+
 });
